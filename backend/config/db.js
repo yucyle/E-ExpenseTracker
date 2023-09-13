ff0e3a31be112c966820f0932b8e675ca7c0046c
levanduc <a519levanduc@gmail.com>
2023-09-13 04:28:27 -0500

    Commit

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,7 +2,6 @@ const mongoose = require('mongoose');
 const db = async () => {
     try {
         // mongoose.set('strictQuery', false);
-        console.log(process.env.MONGO_URL);
         await mongoose.connect(process.env.MONGO_URL);
         console.log('Database connected!');
     } catch (err) {
